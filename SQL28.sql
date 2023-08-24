2afc03405271fbe75d8bc0470ee8aa31d0284cea
GitHubBladeRunner <64316233+GitHubBladeRunner@users.noreply.github.com>
2023-08-24 20:23:55 +0300

    Create SQL28.sql

---
+++ SQL28.sql
@@ -0,0 +1,4 @@
+SELECT TOP (3) ShipCountry, AVG(Freight) AS AverageFreight FROM Orders
+WHERE OrderDate BETWEEN (SELECT DATEADD(YEAR, -1, MAX(OrderDate)) FROM Orders) AND (SELECT MAX(OrderDate) FROM Orders)
+GROUP BY ShipCountry
+ORDER BY AverageFreight DESC
